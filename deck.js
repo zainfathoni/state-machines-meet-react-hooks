d4163241a79fa7004311ae2db9969efb39ece881
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-10 23:51:13 +0700

    Add Hierarchical State introduction

--- deck.js
+++ deck.js
@@ -3,5 +3,13 @@ export { nightOwlFull as theme } from 'code-surfer/themes'
 import { slides as example } from './example.mdx'
 import { slides as problem } from './problem.mdx'
 import { slides as solution } from './solution.mdx'
+import { slides as hierarchy } from './hierarchy.mdx'
+import { slides as guards } from './guards.mdx'
 
-export const slides = [...problem, ...example, ...solution]
+export const slides = [
+  ...problem,
+  ...example,
+  ...solution,
+  ...hierarchy,
+  ...guards,
+]
