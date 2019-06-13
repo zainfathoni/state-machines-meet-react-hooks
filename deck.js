abdda8272b787d55bcfc9e089107604ee78f8f4f
Zain Fathoni <zain.fathoni@gmail.com>
2019-06-14 01:19:33 +0800

    Remove problem slides & rename example slides into toggle slides

--- deck.js
+++ deck.js
@@ -10,20 +10,18 @@ import { slides as demo3 } from './demo-3.mdx'
 import { slides as demo4 } from './demo-4.mdx'
 import { slides as demo5 } from './demo-5.mdx'
 import { slides as demo6 } from './demo-6.mdx'
-import { slides as example } from './example.mdx'
+import { slides as toggle } from './toggle.mdx'
 import { slides as guards2 } from './guards-2.mdx'
 import { slides as guards } from './guards.mdx'
 import { slides as hierarchy } from './hierarchy.mdx'
 import { slides as interpreter } from './interpreter.mdx'
 import { slides as introduction } from './introduction.mdx'
-import { slides as problem } from './problem.mdx'
 import { slides as solution } from './solution.mdx'
 import { slides as useMachine } from './use-machine.mdx'
 
 export const slides = [
   ...introduction,
-  ...problem,
-  ...example,
+  ...toggle,
   ...demo1,
   ...solution,
   ...demo2,
@@ -36,5 +34,5 @@ export const slides = [
   ...guards,
   ...guards2,
   ...demo6,
-  ...conclusion
+  ...conclusion,
 ]
